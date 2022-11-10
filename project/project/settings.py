bb00d583b22f0b0dfe60e76146fdda193a22e599
Seno <senoardi01@gmail.com>
2022-11-10 21:43:11 +0700

    oke

--- project/project/settings.py
+++ project/project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@rv5e7$r8i8n$d9ir35@8x+%lev=45ydy6yy6=$9(4#ph2miv=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['']
 
 
 # Application definition
@@ -116,6 +116,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'assets'),
+# )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- project/project/urls.py
+++ project/project/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path
+# from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", views.index),
+    path("", views.welcome),
 ]
+
+
+# urlpatterns += staticfiles_urlpatterns()
